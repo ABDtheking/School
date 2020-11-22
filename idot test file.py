be24ac61963a0991205dfce2720126f5582c17df
ABDtheking <70889286+ABDtheking@users.noreply.github.com>
2020-11-22 18:18:34 +0200

    a

--- idot test file.py
+++
@@ -1,8 +0,0 @@
-temp = float(input("Please enter todays temperatore: "))
-if (temp >= 35 ):
-    print("שרב כבד")
-
-first_num = float(input("Please enter the first number: "))
-second_num = float(input("Please enter the second number: "))
-if (first_num > second_num):
-    print("המספר הראשון גדול יותר")
