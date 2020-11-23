448919bdc0c6b61f8bc543397af8fc06089b193e
ABDtheking <70889286+ABDtheking@users.noreply.github.com>
2020-11-23 19:42:30 +0200

    Create a.py

---
+++ a.py
@@ -0,0 +1,14 @@
+blue = "\033[34m"
+red = "\33[31m"
+
+print(f"""{blue}
+ ██{red}░ {blue}██{red} ▓{blue}█████  ██{red}▓     {blue}██{red}▓     {red}▒{blue}█████  
+{red}▓{blue}██{red}░ {blue}██{red}▒▓{blue}█   ▀ {red}▓{blue}██{red}▒    ▓{blue}██{red}▒    ▒{blue}██{red}▒  {blue}██{red}▒
+▒{blue}██▀▀██{red}░▒{blue}███   {red}▒{blue}██{red}░    {red}▒{blue}██{red}░    {red}▒{blue}██{red}░  {blue}██{red}▒
+░▓{blue}█ {red}░{blue}██ {red}▒▓{blue}█  ▄ {red}▒{blue}██{red}░    {red}▒{blue}██{red}░    {red}▒{blue}██   ██{red}░
+░▓{blue}█{red}▒░{blue}██{red}▓░▒{blue}████{red}▒░{blue}██████{red}▒░{blue}██████{red}▒░ {blue}████{red}▓▒░
+ ▒ ░░▒░▒░░ ▒░ ░░ ▒░▓  ░░ ▒░▓  ░░ ▒░▒░▒░ 
+ ▒ ░▒░ ░ ░ ░  ░░ ░ ▒  ░░ ░ ▒  ░  ░ ▒ ▒░ 
+ ░  ░░ ░   ░     ░ ░     ░ ░   ░ ░ ░ ▒  
+ ░  ░  ░   ░  ░    ░  ░    ░  ░    ░ ░  
+""")
